efe6a0ab89b7be722308837a62462ecc78557ef5
masud-k <contact.masud99@gmail.com>
2024-06-29 20:57:22 +0600

    home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,24 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div>
-      <div className="hero bg-base-200 ">
+      <div className="hero -mt-16 ">
         <div className="hero-content text-center">
           <div className="max-w-lg">
-            <h1 className="text-4xl font-bold">Welcome to ByteBlaze</h1>
+            <h1 className="text-4xl font-bold">Welcome to <span className="bg-gradient-to-r from-primary via-blue-500 to-secondary text-transparent bg-clip-text bg-300% animate-gradient">ByteBlaze</span></h1>
             <p className="py-6">
               ByteBlaze is a cutting-edge technology web page dedicated the latest news, trends, and insights in the tech industry.
             </p>
             <div className="flex gap-2 justify-center">
-              <Link to='/blogs'>Read blogs</Link>
-              <Link to='/bookmarks'>Bookmarks</Link>
+              <Link to='/blogs' className="relative inline-block px-4 py-2 font-medium group">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                <span className="relative text-black group-hover:text-white">Read Blogs</span>
+              </Link>
+              <Link to='/blogs' className="relative inline-block px-4 py-2 font-medium group">
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                <span className="relative text-black group-hover:text-white">Bookmarks</span>
+              </Link>
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import wave from "../assets/wave.svg";
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+        <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-116px)]">
            <Hero/>
+           <img className="absolute bottom-0 w-full" src={wave} alt="" />
         </div>
     );
 };
