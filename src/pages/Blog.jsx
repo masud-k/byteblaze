8d051fec91f1647d94e652584e64a95cfdac882e
masud-k <contact.masud99@gmail.com>
2024-07-13 21:22:17 +0600

    Loader added

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -27,7 +27,7 @@ const Blog = () => {
                         <Link
                             to=''
                             onClick={() => setTabindex(0)}
-                            className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
+                            className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 0 ? 'border border-b-0 bg-blue-400' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                             </svg>
@@ -36,7 +36,7 @@ const Blog = () => {
                         <Link
                             to={'author'}
                             onClick={() => setTabindex(1)}
-                            className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
+                            className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabindex === 1 ? 'border border-b-0 bg-blue-400' : 'border-b'} rounded-t-lg border-gray-600 text-gray-900`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                 <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                 <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
