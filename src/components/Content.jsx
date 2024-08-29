f0a807573bb69de9a52d7a58d8bf3495e1ea2267
masud-k <contact.masud99@gmail.com>
2024-08-29 21:47:01 +0600

    link add

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,7 @@ import rehypeRaw from "rehype-raw";
 const Content = () => {
 
     const blog = useLoaderData()
-    const { cover_image, title, tags, body_html } = blog
+    const { cover_image, title, tags, body_html, url } = blog
     console.log(blog)
 
     return (
@@ -21,7 +21,7 @@ const Content = () => {
 
             </div>
             <div className=" space-y-2 ">
-                <h3 className=" text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                <a href={url} target='_blank' className=" text-2xl font-semibold group-hover:underline group-focus:underline">{title}</a>
                 <Markdown rehypePlugins={[rehypeRaw]}>{body_html}</Markdown>
 
             </div>
